559f37ecf0c05266e5fa8bc0acd9050b078c2392
TenetNoctis <noctis.eden@gmail.com>
2019-01-16 17:21:02 +0500

    up

---
+++ src/testfourb/TestFourB.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testfourb;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author User
+ */
+public class TestFourB {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) throws IOException
+  {
+    Scanner textfile = new Scanner (System.in);
+    BufferedReader bf = null;
+    String line = "";
+    String txtSplitby = "-";
+    int counter = 0;
+    List<String> digit = new ArrayList<>();
+    List<String> space = new ArrayList<>();
+   
+    String txt = "strings.txt";
+    bf = new BufferedReader (new FileReader(txt));
+      line=bf.readLine();
+      String[] splitLine = line.split(txtSplitby);
+      for (int i = 0; i < splitLine.length; i++) {
+          String name = splitLine[i];
+          boolean hasDigit = name.matches(".*\\d+.*");
+          if (hasDigit == true) {
+              digit.add(name);
+          }
+          boolean hasWhiteSpace = name.contains(" ");
+          if (hasWhiteSpace == true) {
+              space.add(name);
+          }
+      }
+      System.out.println(digit);
+      System.out.println(space);
+    }
+        
+}
+  
+
+  
